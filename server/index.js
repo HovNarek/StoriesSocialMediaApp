3c61d5c9be6c942886717a4e3ce625e808259d5d
HovNarek <narek-96@list.ru>
2023-04-06 17:33:03 +0400

    check server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -20,10 +19,12 @@ app.use("/user", userRouter);
 
 const PORT = process.env.PORT|| 5000;
 console.log('serverrrrrrr');
-console.log('port', process.env.PORT);
-console.log('connection_URL', process.env.CONNECTION_URL);
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+// console.log('port', process.env.PORT);
+// console.log('connection_URL', process.env.CONNECTION_URL);
+console.log('end consoles');
 
-mongoose.set('useFindAndModify', false);
+// mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
+//
+// mongoose.set('useFindAndModify', false);
