f46caeb6dcde08ed67d86169c0169aec113bf8bf
HovNarek <narek-96@list.ru>
2023-04-06 17:06:59 +0400

    check server

--- server/index.js
+++ server/index.js
@@ -19,7 +19,9 @@ app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
 const PORT = process.env.PORT|| 5000;
-
+console.log('serverrrrrrr');
+console.log('port', process.env.PORT);
+console.log('connection_URL', process.env.CONNECTION_URL);
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
