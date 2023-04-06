56ddf344eb9107840347c6589b2e32a6df85c741
HovNarek <narek-96@list.ru>
2023-04-06 17:50:30 +0400

    check server

--- server/index.js
+++ server/index.js
@@ -1,41 +1,28 @@
-//
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-//
-// import postRoutes from './routes/posts.js';
-// import userRouter from "./routes/user.js";
-//
-// const app = express();
-// // dotenv.config()
-//
-// app.use(express.json({ limit: '30mb', extended: true }))
-// app.use(express.urlencoded({ limit: '30mb', extended: true }))
-// app.use(cors());
-//
-// app.use('/posts', postRoutes);
-// app.use("/user", userRouter);
-//
-// // const PORT = process.env.PORT|| 5000;
-// console.log('serverrrrrrr');
-// // console.log('port', process.env.PORT);
-// // console.log('connection_URL', process.env.CONNECTION_URL);
-// console.log('end consoles');
-//
-// // mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-// //   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-// //   .catch((error) => console.log(`${error} did not connect`));
-// //
-// // mongoose.set('useFindAndModify', false);
-
 
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
+dotenv.config()
 
-app.get("/", (req, res) => {
-    res.send('hello from express')
-})
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
 
 export default app
