54c14611808ac537b732f93cd77f04db0cd33169
HovNarek <narek-96@list.ru>
2023-04-06 17:34:45 +0400

    check server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
-dotenv.config()
+// dotenv.config()
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const PORT = process.env.PORT|| 5000;
+// const PORT = process.env.PORT|| 5000;
 console.log('serverrrrrrr');
 // console.log('port', process.env.PORT);
 // console.log('connection_URL', process.env.CONNECTION_URL);
